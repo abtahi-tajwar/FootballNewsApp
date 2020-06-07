f37a049e950caea44dcd483106fb50a6e2150554
Abtahi Tajwar <abtahitajwar@gmail.com>
2020-06-07 15:25:33 +0600

    Mobile view completed upto select league section

--- collapsibleMenu.js
+++ collapsibleMenu.js
@@ -8,3 +8,26 @@ drop.addEventListener("click", () => {
 closedrop.addEventListener("click", () => {
     dropDownDiv.style.height = "0px";
 });
+
+var select_league_dropdown = document.querySelector(".select_league_dropdown");
+var dropdown_click_area = document.querySelector(".dropdown_click_area");
+let dropdown_open_state = false;
+if(select_league_dropdown != null) {
+    dropdown_click_area.addEventListener("click", () => {
+        if(dropdown_open_state) {
+            select_league_dropdown.style.height = "33px";
+            dropdown_open_state = !dropdown_open_state;
+        }
+        else {
+            select_league_dropdown.style.height = window.innerHeight - getOffset(select_league_dropdown).top + "px";
+            dropdown_open_state = !dropdown_open_state;
+        }
+    });
+}
+function getOffset(element) {
+    const rect = element.getBoundingClientRect();
+    return {
+        left: rect.left,
+        top: rect.top
+    }
+}
