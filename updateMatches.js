8e9daeaa4595812c65dfd42c9f7646613196a006
Abtahi Tajwar <abtahitajwar@gmail.com>
2020-06-10 22:05:25 +0600

    Prediction container showed

--- updateMatches.js
+++ updateMatches.js
@@ -2,7 +2,8 @@ let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 let tempDate = "";
 let warning = document.querySelector(".warning");
 let leagueDropDownItem = document.querySelectorAll(".select_league-container ul li");
-console.log(leagueDropDownItem);
+let predictionPanel = document.querySelector(".prediction_container");
+let matchCard = new Array();
 
 function fetchFixture(leagueID) {
     fetch("https://api-football-v1.p.rapidapi.com/v2/fixtures/league/"+leagueID+"/next/25?timezone="+timezone, {
@@ -26,14 +27,82 @@ function fetchFixture(leagueID) {
             updateNextMatch(data.api.fixtures[0].homeTeam.team_name, data.api.fixtures[0].awayTeam.team_name,data.api.fixtures[0].homeTeam.logo, data.api.fixtures[0].awayTeam.logo, data.api.fixtures[0].event_date);
             updateFixtures(data.api.fixtures);
         }
-        
+        return data;     
 
+    }).then(data => {
+        matchCard = document.querySelectorAll(".match_card");
+        //..
+        console.log(matchCard);
+        matchCard.forEach((card, i) => {
+            card.addEventListener("click", () => {
+                predictionContainerOpen(data.api.fixtures[i].fixture_id, data.api.fixtures[0].homeTeam.logo, data.api.fixtures[0].awayTeam.logo);
+            });
+        });
     })
     .catch(err => {
         console.log(err);
     });
 }
 //fetchFixture(524);
+
+function predictionContainerOpen(id, homeLogo, awayLogo)
+{
+    predictionPanel.style.height = "500px";
+    let predictionContent = document.querySelector(".prediction_container");
+    fetch("https://api-football-v1.p.rapidapi.com/v2/predictions/"+id, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+            "x-rapidapi-key": "f7c349acecmsh9b10dfe4084b189p1b054cjsn114ad81fc120"
+        }
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        // data.api.predictions[0].teams.home.
+        document.querySelector(".prediction_home_team img").src = homeLogo;
+        document.querySelector(".prediction_away_team img").src = awayLogo;
+        document.querySelector(".prediction_home_team_name").innerHTML = data.api.predictions[0].teams.home.team_name;
+        document.querySelector(".prediction_away_team_name").innerHTML = data.api.predictions[0].teams.away.team_name;
+        document.querySelector(".prediction_score").innerHTML = Math.round(data.api.predictions[0].goals_home)*-1 + ":" + Math.round(data.api.predictions[0].goals_away)*-1;
+        
+        //Setting winning chance bar and text
+        document.querySelector(".home-winchance p").innerHTML = data.api.predictions[0].winning_percent.home;
+        document.querySelector(".home-winchance").style.width = data.api.predictions[0].winning_percent.home;
+        document.querySelector(".away-winchance p").innerHTML = data.api.predictions[0].winning_percent.away;
+        document.querySelector(".away-winchance").style.width = data.api.predictions[0].winning_percent.away;
+        document.querySelector(".draw-chance p").innerHTML = data.api.predictions[0].winning_percent.draws;
+        document.querySelector(".draw-chance").style.width = data.api.predictions[0].winning_percent.draws;
+
+        //Setting stat
+            //Home
+        document.querySelector(".prediction-home-possession .stat-percentage").innerHTML = data.api.predictions[0].comparison.forme.home;
+        document.querySelector(".prediction-home-possession .stat_bar").style.width = data.api.predictions[0].comparison.forme.home;
+        //
+        document.querySelector(".prediction-home-att .stat-percentage").innerHTML = data.api.predictions[0].comparison.att.home;
+        document.querySelector(".prediction-home-att .stat_bar").style.width = data.api.predictions[0].comparison.att.home;
+        //
+        document.querySelector(".prediction-home-def .stat-percentage").innerHTML = data.api.predictions[0].comparison.def.home;
+        document.querySelector(".prediction-home-def .stat_bar").style.width = data.api.predictions[0].comparison.def.home;
+            //Away
+        document.querySelector(".prediction-away-possession .stat-percentage").innerHTML = data.api.predictions[0].comparison.forme.away;
+        document.querySelector(".prediction-away-possession .stat_bar").style.width = data.api.predictions[0].comparison.forme.away;
+        //
+        document.querySelector(".prediction-away-att .stat-percentage").innerHTML = data.api.predictions[0].comparison.att.away;
+        document.querySelector(".prediction-away-att .stat_bar").style.width = data.api.predictions[0].comparison.att.away;
+        //
+        document.querySelector(".prediction-away-def .stat-percentage").innerHTML = data.api.predictions[0].comparison.def.away;
+        document.querySelector(".prediction-away-def .stat_bar").style.width = data.api.predictions[0].comparison.def.away;
+    })
+    .then(() => {
+        predictionContent.style.display = "block";
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+}
 
 function updateNextMatch(home, away, homeLogo, awayLogo, dateTime)
 {
@@ -136,6 +205,12 @@ leagueDropDownItem.forEach((Item) => {
         let leagueID = event.target.attributes["tag"].value;
         console.log(leagueID);
         fetchFixture(leagueID);
-        toggleLeagueDropdown();
+        
+        //..
+        if(matchCard !== null) console.log(matchCard);
     });
-})
+});
+document.querySelector(".prediction_minimize").addEventListener("click", () => {
+    document.querySelector(".prediction_container").style.display = "none";
+    predictionPanel.style.height = "0px";
+});
